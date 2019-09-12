50511a2f6a66867e9a8f720365ebb74cab0637d3
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-12 18:03:38 -0400

    rollback migration for articles description property

--- alhpa-blog/db/migrate/20190912215252_create_articles.rb
+++ alhpa-blog/db/migrate/20190912215252_create_articles.rb
@@ -4,7 +4,6 @@ class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
       t.string :title
-      t.text :description
     end
   end
 end
--- alhpa-blog/db/schema.rb
+++ alhpa-blog/db/schema.rb
@@ -10,11 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_215252) do
-
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
