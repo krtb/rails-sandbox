ab63bea45f9ef56596a3f65841a3f8a49cfd16b8
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-12 18:04:28 -0400

    create db migration with only title

--- alhpa-blog/db/schema.rb
+++ alhpa-blog/db/schema.rb
@@ -10,6 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_09_12_215252) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+  end
 
 end
