6ffa990737969f1fa460e9b06f8fdd3de1ee2a5d
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-13 12:33:30 -0400

    add time stamp column changes manually, could have done with t.timestamps

--- alhpa-blog/db/migrate/20190913161000_add_description_to_articles.rb
+++ alhpa-blog/db/migrate/20190913161000_add_description_to_articles.rb
@@ -4,7 +4,12 @@ class AddDescriptionToArticles < ActiveRecord::Migration[6.0]
   # 1) give the table name = articles
   # 2) give attributes = description
   # 3) type for attr name = text
+
+  #TIMESTAMPS
+  #  - normally added to tables by using t.timestamps
   def change
     add_column :articles, :description, :text 
+    add_column :articles, :created_at, :datetime
+    add_column :articles, :updated_at, :datetime
   end
 end
