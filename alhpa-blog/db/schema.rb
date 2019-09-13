a2bbe9b5dcb881f3588ce075be23e26c05027f37
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-13 12:41:54 -0400

    ran migrations for description and datetime

--- alhpa-blog/db/schema.rb
+++ alhpa-blog/db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_215252) do
+ActiveRecord::Schema.define(version: 2019_09_13_161000) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
