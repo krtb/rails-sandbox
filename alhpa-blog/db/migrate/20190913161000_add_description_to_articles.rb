1dba0d911fabf58c384425e6560edb23b71b1e85
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-13 12:25:00 -0400

    use add column, pass in table name/ attribute / type for attr

--- alhpa-blog/db/migrate/20190913161000_add_description_to_articles.rb
+++ alhpa-blog/db/migrate/20190913161000_add_description_to_articles.rb
@@ -1,5 +1,10 @@
 class AddDescriptionToArticles < ActiveRecord::Migration[6.0]
   #change method is how we're going to update the table
+  # use add_column
+  # 1) give the table name = articles
+  # 2) give attributes = description
+  # 3) type for attr name = text
   def change
+    add_column :articles, :description, :text 
   end
 end
