da6c6f87b7ce7661aca4e5b714c4bf600a02b891
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-17 10:20:18 -0400

    start mini auth project, add readme file

---
+++ authenticator/ReadMe.md
@@ -0,0 +1,6 @@
+# This Covers
+* Hash
+* Array
+* Branching
+* While loops
+* Designing Program execution flow
---
+++ authenticator/database.rb
@@ -0,0 +1,12 @@
+users = [
+    { username: 'kurt', password: 'password1' },
+    { username: 'andrea', password: 'password2' },
+    { username: 'tim', password: 'password3' },
+    { username: 'roberto', password: 'password4' },
+    { username: 'maria', password: 'password5' },
+]
+puts "Welcome to the authenticator"
+25.times { print "_" }
+puts
+puts "This program takes input from user and compare password."
+puts "If password is correct, will puts password."
