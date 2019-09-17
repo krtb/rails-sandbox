36d5178dec0b4aa9ee6be4ca07e806de0bbf34bd
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-17 14:51:00 -0400

    create root route to render home page on initialization

--- alhpa-blog/config/routes.rb
+++ alhpa-blog/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get path directed to welcome controller, home action
-  get 'pages/home', to: 'pages#home'
+  root 'pages#home'
   get 'pages/about', to: 'pages#about'
 end
