953fe918a9ada7aaf8832d17bd68a4bba6ee7316
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-27 10:24:09 -0500

    render obj after adding text, submitting

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -4,6 +4,6 @@
     end
 
     def create
-    
+        render plain: params[:article].inspect # <ActionController::Parameters {"title"=>"hello", "description"=>"there!\r\n"} permitted: false>
     end
  end
