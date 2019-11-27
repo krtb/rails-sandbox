9d470352c2302cd8818c52dbfea5c5bd49a05676
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-27 10:59:09 -0500

    redirect_to show route after creation of @article

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -7,6 +7,7 @@
         # render plain: params[:article].inspect # <ActionController::Parameters {"title"=>"hello", "description"=>"there!\r\n"} permitted: false>
         @article = Article.new(article_params) #need to white list values of Article, with what's beeing passed in from params  hash
         @article.save
+        redirect_to article_show(@article)
     end
 
     private
