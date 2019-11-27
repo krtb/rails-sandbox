e02522c42577405e931e1edad04dd358553119e2
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-27 10:47:23 -0500

    comment out rendering of Article hash

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -4,12 +4,12 @@
     end
 
     def create
-        render plain: params[:article].inspect # <ActionController::Parameters {"title"=>"hello", "description"=>"there!\r\n"} permitted: false>
+        # render plain: params[:article].inspect # <ActionController::Parameters {"title"=>"hello", "description"=>"there!\r\n"} permitted: false>
         @article = Article.new(article_params) #need to white list values of Article, with what's beeing passed in from params  hash
         @article.save
     end
 
     private
 
-    
+
  end
