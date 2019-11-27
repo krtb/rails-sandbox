72a99d32b3d37331fbfdcc16f29c8b2f8c33dae0
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-27 10:26:21 -0500

    create new Article, with params_method

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -5,5 +5,6 @@
 
     def create
         render plain: params[:article].inspect # <ActionController::Parameters {"title"=>"hello", "description"=>"there!\r\n"} permitted: false>
+        @article = Article.new(article_params)
     end
  end
