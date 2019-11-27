b79c3d4ae932671648a6ee72cf48c327e514c644
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-27 11:14:11 -0500

    add if/else, flash success || re-render new view

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -6,8 +6,12 @@
     def create
         # render plain: params[:article].inspect # <ActionController::Parameters {"title"=>"hello", "description"=>"there!\r\n"} permitted: false>
         @article = Article.new(article_params) #need to white list values of Article, with what's beeing passed in from params  hash
-        @article.save
-        redirect_to article_path(@article) # article GET /articles/:id article#show
+        if @article.save
+            flash[:notice] = "Article was successfully created" #show message to user
+            redirect_to article_path(@article) # article GET /articles/:id article#show
+        else
+            render :new #render new view, to give user change to try again
+        end
     end
 
     private
