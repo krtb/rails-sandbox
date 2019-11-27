66d30cafbb1620a9d7485e61db5a0bf45519f25d
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-27 12:21:01 -0500

    define edit action method in Article Controller

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@
         @article = Article.new
     end
 
+    def edit
+    
+    end
+
     def create
         # render plain: params[:article].inspect # <ActionController::Parameters {"title"=>"hello", "description"=>"there!\r\n"} permitted: false>
         @article = Article.new(article_params) #need to white list values of Article, with what's beeing passed in from params  hash
