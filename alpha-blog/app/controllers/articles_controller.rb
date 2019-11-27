1b8bdd892da198e57ab3aa6a9afeb9c2b3f2b7b6
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-27 10:28:14 -0500

    add note to create whitelist for params hash

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -5,6 +5,6 @@
 
     def create
         render plain: params[:article].inspect # <ActionController::Parameters {"title"=>"hello", "description"=>"there!\r\n"} permitted: false>
-        @article = Article.new(article_params)
+        @article = Article.new(article_params) #need to white list values of Article, with what's beeing passed in from params  hash
     end
  end
